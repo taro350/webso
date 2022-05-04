a5080e927885ac787907c98d919b4cec85d2af26
Taro Saika <tarosaika@Taros-MacBook-Pro.local>
2022-05-04 14:11:56 +0900

    Alpaca API for market data

---
+++ api/stickers.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/alpacahq/alpaca-trade-api-go/alpaca"
+	"github.com/alpacahq/alpaca-trade-api-go/v2"
+)
+
+func init() {
+	alpaca.SetBaseUrl("https://data.alpaca.markets")
+}
+
+func main() {
+	bars := alpaca.GetBars(
+		"AAPL", v2.Day, v2.Raw, time.Now().Add(-7*24*time.Hour), time.Now().Add(-20*time.Minute), 7)
+	var barset []v2.Bar
+
+	for bar := range bars {
+		if bar.Error != nil {
+			panic(bar.Error)
+		}
+		barset = append(barset, bar.Bar)
+	}
+
+	// See how much AAPL moved in that timeframe.
+	startPrice := barset[0].Open
+	endPrice := barset[len(barset)-1].Close
+	percentChange := ((endPrice - startPrice) / startPrice) * 100
+	fmt.Printf("AAPL moved %v%% over the last 7 days.", percentChange)
+}
--- go.mod
+++ go.mod
@@ -7,3 +7,5 @@ require (
 	github.com/gorilla/mux v1.8.0
 	github.com/gorilla/websocket v1.5.0
 )
+
+require github.com/alpacahq/alpaca-trade-api-go v1.9.0 // indirect
